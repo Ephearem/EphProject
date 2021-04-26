be1b84a1b2cba540204295cb9fb12f4444d5d03b
Eph <Eph@DESKTOP-M8JVNFC>
2021-04-26 19:01:07 +0300

    Implement logic to initialize GLFW, GLAD, and to create a window This logic is implemented in the 'init_window' method of the 'Core' class.

--- EphProject/src/core/Core.cpp
+++ EphProject/src/core/Core.cpp
@@ -13,7 +13,11 @@
 
 /** @includes  -------------------------------------------------------------**/
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include "Core.hpp"
+#include "Log.hpp"
 
 
 
@@ -34,8 +38,87 @@
 ----------------------------------------------------------------------------**/
 Core& Core::instance()
 {
-	static Core instance_;
-	return instance_;
+    static Core instance_;
+    return instance_;
+}
+
+
+/**----------------------------------------------------------------------------
+; @func init_window
+;
+; @brief
+;   Initialize GLFW, GLAD. Create a window.
+;
+; @params
+;   window_title    | Title of the window to be created.
+;   window_size     | The size of the window (in pixels) to be created.
+;   is_full_screen  | Whether to enable full screen mode after window creation.
+;   swap_interval   | The minimum number of screen updates to wait for until
+;                   | the buffers are swapped by glfwSwapBuffers.
+;
+; @return
+;   None
+;
+; // TODO: return GLFWwindow*     | GLFW window object pointer.
+;
+----------------------------------------------------------------------------**/
+void Core::init_window(char const* window_title, glm::ivec2 const& window_size,
+                       bool is_full_screen, int swap_interval)
+{
+    GLFWmonitor* monitor_ptr = nullptr;
+
+    glfwInit();                         /* Initialize and configure GLFW     */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    if (is_full_screen)
+    {
+        monitor_ptr = glfwGetPrimaryMonitor();
+    }
+    this->window_ptr_ = glfwCreateWindow(window_size.x, window_size.y,
+        window_title, monitor_ptr, nullptr);
+                                        /* Create the window                 */
+    if (this->window_ptr_ == nullptr)
+    {
+        LOG_ERROR("Failed to create GLFW window");
+        glfwTerminate();
+        exit(-1);
+    }
+    glfwMakeContextCurrent(this->window_ptr_);
+    glfwSwapInterval(swap_interval);    /* Set the number of screen updates  */
+                                        /* to wait from the time
+                                        /* glfwSwapBuffers was called before */
+                                        /* swapping the buffers and          */
+                                        /* returning (i.e. vsync)            */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {                                   /* Initialize GLAD                   */
+        LOG_ERROR("Failed to initialize GLAD");
+        exit(-1);
+    }
+    glViewport(0, 0, window_size.x, window_size.y);
+    glEnable(GL_BLEND);                 /* Enable GL_BLEND to support        */
+                                        /* transparent textures              */
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+
+/**----------------------------------------------------------------------------
+; @func get_window_ptr
+;
+; @brief
+;   Returns a pointer to the created GLFW window object.
+; 
+; @params
+;   None
+;
+; @return
+;   GLFWwindow * 
+;
+----------------------------------------------------------------------------**/
+GLFWwindow* Core::get_window_ptr() const
+{
+    return this->window_ptr_;
 }
 
 
@@ -46,10 +129,13 @@ Core& Core::instance()
 ;   Constructor.
 ;
 ; @params
+;   None
 ;
 ; @return
+;   None
 ;
 ----------------------------------------------------------------------------**/
 Core::Core()
+    :window_ptr_(nullptr), window_size_(0)
 {
 }
--- EphProject/src/core/Core.hpp
+++ EphProject/src/core/Core.hpp
@@ -14,6 +14,17 @@
 
 
 
+/** @includes  -------------------------------------------------------------**/
+
+#include <glm/vec2.hpp>
+
+
+
+/** @type_declarations -----------------------------------------------------**/
+
+struct GLFWwindow;
+
+
 /** @classes ---------------------------------------------------------------**/
 
 class Core
@@ -21,7 +32,15 @@ class Core
 public:
     static Core& instance();
 
+    void init_window(char const* window_title, glm::ivec2 const& window_size,
+                     bool is_full_screen, int swap_interval);
+
+    GLFWwindow* get_window_ptr() const;
+
 private:
+    GLFWwindow* window_ptr_;
+    glm::ivec2 window_size_;
+
     Core();
     Core(const Core& root) = delete;
     Core& operator=(const Core&) = delete;
