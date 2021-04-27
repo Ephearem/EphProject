b76b36554c3ecacf15c16a20526936c898f1c497
Eph <Eph@DESKTOP-M8JVNFC>
2021-04-27 18:34:49 +0300

    Draw a colored rectangle A rectangle is formed from the vertices of two triangles. The coordinates of the vertices are specified in the 'vertices' array, which is placed in the 'vertex_buffer'. The vertices to be drawn and the order of vertex joining are specified in the 'indices' array, which is placed in the 'indices_buffer' buffer. The color fills the quad in a fragment shader. Drawing is implemented in the render loop by calling the 'glDrawElements' function.

--- EphProject/src/main.cpp
+++ EphProject/src/main.cpp
@@ -48,6 +48,8 @@ void main_loop_iteration()
 int main(int argc, char** argv)
 {
     Core::instance().init_window("Eph Project", { 800,600 }, false, 0);
+    Core::instance().init_shaders("src/core/shaders/default_vertex.shader",
+        "src/core/shaders/default_fragment.shader");
     Core::instance().start_main_loop(main_loop_iteration);
     return 0;
 }
